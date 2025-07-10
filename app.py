7c234ef97959607106adf84d832333dac5b004aa
gopinath2866 <gopi0nath2929@gmail.com>
2025-07-10 15:03:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,5 +72,9 @@ def journal():
     entries = load_all_journals()
     return render_template("journal.html", entries=entries)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
